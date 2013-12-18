c84a1bc2fff96a845f86fdabb5a2b8fa964c513a
Jared Forsyth <jared@jaredforsyth.com>
2013-12-17 21:37:29 -0700

    first wip

--- webapp.js
+++ webapp.js
@@ -1,27 +1,49 @@
-var jobHandler = require('./lib/handler')
+var setStatus = require('./lib/handler')
 
 function jobStatus(job) {
   if (job.errored) return 'error'
   return job.test_exitcode === 0 ? 'success' : 'failure'
 }
 
+function getToken(models, projectName, done) {
+  models.Project.findOne({name: projectName}, function (err, project) {
+    if (err) return done(err)
+    models.User.findById(project.creator, function (err, user) {
+      if (err) return done(err)
+      var account = user.account(project.provider)
+      if (!account) {
+        return done(new Error('no account found for ' + user._id))
+      }
+      if (!account.accessToken) {
+        return done(new Error('account misconfigured; no access token ' + user._id))
+      }
+      done(null, account.accessToken)
+    })
+  })
+}
+
 module.exports = {
   // global events
   listen: function (io, context) {
-    io.on('plugin.github-status.started', function (jobId) {
-      console.log('started', jobId)
+    io.on('plugin.github-status.started', function (jobId, projectName, data) {
+      getToken(context.models, projectName, function (err, token) {
+        if (err) return console.error('Failed to get access token', err.message)
+        var url = context.config.server_name + '/' + projectName + '/job/' + jobId
+        setStatus(token, url, data, 'pending')
+      })
     })
 
-    io.on('plugin.github-status.done', function (jobId, pluginConfig) {
+    io.on('plugin.github-status.done', function (jobId, pluginConfig, data) {
       var onDoneAndSaved = function (job) {
-        console.log('doneandsaved', job)
-        /*
-        if (job._id.toString() === jobId.toString()) {
-          io.removeListener('job.doneAndSaved', onDoneAndSaved)
-          context.pluginConfig = pluginConfig
-          jobHandler(job, context, jobStatus(job))
-        }
-        */
+        if (job._id.toString() !== jobId.toString()) return
+        console.log('yeah', job.project)
+
+        io.removeListener('job.doneAndSaved', onDoneAndSaved)
+        getToken(context.models, job.project, function (err, token) {
+          if (err) return console.error('Failed to get access token', err.message)
+          var url = context.config.server_name + '/' + job.project + '/job/' + jobId
+          setStatus(token, url, data, jobStatus(job))
+        })
       }
       io.on('job.doneAndSaved', onDoneAndSaved)
     })
--- worker.js
+++ worker.js
@@ -2,10 +2,13 @@ module.exports = {
   init: function (config, job, context, callback) {
     return callback(null, {
       listen: function (emitter) {
-        console.log(job)
-        emitter.emit('plugin.github-status.started', job._id)
+        if (!job.plugin_data || !job.plugin_data.pull_request) {
+          console.log(context)
+          return // context.comment('No pull request data found')
+        }
+        emitter.emit('plugin.github-status.started', job._id, job.project, job.plugin_data.pull_request)
         emitter.once('job.status.tested', function (jobId) {
-          emitter.emit('plugin.github-status.done', jobId, config)
+          emitter.emit('plugin.github-status.done', jobId, job.project, job.plugin_data.pull_request)
         })
       }
     })
