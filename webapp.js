b66642f7483d89ea19b708d73fc97c95c065b3f0
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-20 13:56:58 +0200

    Fix lint

--- webapp.js
+++ webapp.js
@@ -1,39 +1,39 @@
-var setStatus = require('./lib/handler')
-  , debug = require('debug')('strider-github-status')
+var debug = require('debug')('strider-github-status');
+var setStatus = require('./lib/handler');
 
 function jobStatus(job) {
-  if (job.errored) return 'error'
-  return job.test_exitcode === 0 ? 'success' : 'failure'
+  if (job.errored) return 'error';
+  return job.test_exitcode === 0 ? 'success' : 'failure';
 }
 
-// TODO: give information here as to why id errored/failed?
+// TODO: give information here as to why it errored/failed?
 function jobDescription(job) {
-  if (job.errored) return 'Strider tests errored'
-  return 'Strider tests ' + (job.test_exitcode === 0 ? 'succeeded' : 'failed')
+  if (job.errored) return 'Strider tests errored';
+  return 'Strider tests ' + (job.test_exitcode === 0 ? 'succeeded' : 'failed');
 }
 
 module.exports = {
   // global events
   listen: function (io, context) {
     io.on('plugin.github-status.started', function (jobId, projectName, token, data) {
-      debug('got', jobId, projectName, token, data)
-      var url = context.config.server_name + '/' + projectName + '/job/' + jobId
-      setStatus(token, url, data, 'pending', 'Strider test in progress')
-    })
+      debug('got', jobId, projectName, token, data);
+      var url = context.config.server_name + '/' + projectName + '/job/' + jobId;
+      setStatus(token, url, data, 'pending', 'Strider test in progress');
+    });
 
     io.on('plugin.github-status.done', function (jobId, projectName, token, data) {
-      var onDoneAndSaved = function (job) {
-        if (job._id.toString() !== jobId.toString()) return
-        debug('plugin done', jobId, projectName, token, data)
+      function onDoneAndSaved(job) {
+        if (job._id.toString() !== jobId.toString()) return;
+        debug('plugin done', jobId, projectName, token, data);
 
-        io.removeListener('job.doneAndSaved', onDoneAndSaved)
+        io.removeListener('job.doneAndSaved', onDoneAndSaved);
         var url = context.config.server_name + '/' + projectName + '/job/' + jobId
           , status = jobStatus(job)
-          , description = jobDescription(job)
-        setStatus(token, url, data, status, description)
+          , description = jobDescription(job);
+        setStatus(token, url, data, status, description);
       }
-      io.on('job.doneAndSaved', onDoneAndSaved)
-    })
+      io.on('job.doneAndSaved', onDoneAndSaved);
+    });
   }
-}
+};
 
--- worker.js
+++ worker.js
@@ -1,41 +1,41 @@
-var debug = require('debug')('strider-github-status')
+var debug = require('debug')('strider-github-status');
 
 module.exports = {
   init: function (config, job, context, callback) {
-    debug('initing', job._id, job.plugin_data)
+    debug('initing', job._id, job.plugin_data);
     if (!job.plugin_data || !job.plugin_data.github || !job.plugin_data.github.pull_request) {
-      debug('No github PR data', job.plugin_data, job)
-      return callback(null, {})
+      debug('No github PR data', job.plugin_data, job);
+      return callback(null, {});
     } else {
-      debug('found pr!', job.plugin_data.github.pull_request)
+      debug('found pr!', job.plugin_data.github.pull_request);
     }
     var projectName = job.project.name
       , creator = job.project.creator
       , account
-      , token
-    account = creator.account(job.project.provider)
+      , token;
+    account = creator.account(job.project.provider);
     if (!account || !account.config.accessToken) {
-      console.error('Account not found for', job.project.provider)
-      debug(job.project.provider, creator.accounts, account)
-      return
+      console.error('Account not found for', job.project.provider);
+      debug(job.project.provider, creator.accounts, account);
+      return;
     }
-    token = account.config.accessToken
-    debug('Token', account, token)
+    token = account.config.accessToken;
+    debug('Token', account, token);
 
     return callback(null, {
       listen: function (emitter) {
-        debug('listening')
+        debug('listening');
         var github_repo_data = {
           user: job.project.provider.config.owner,
           repo: job.project.provider.config.repo,
-          sha: job.plugin_data.github.pull_request.sha,
-        }
-        emitter.emit('plugin.github-status.started', job._id, projectName, token, github_repo_data)
+          sha: job.plugin_data.github.pull_request.sha
+        };
+        emitter.emit('plugin.github-status.started', job._id, projectName, token, github_repo_data);
         emitter.once('job.status.tested', function (jobId) {
-          debug('job was tested', jobId)
-          emitter.emit('plugin.github-status.done', jobId, projectName, token, github_repo_data)
-        })
+          debug('job was tested', jobId);
+          emitter.emit('plugin.github-status.done', jobId, projectName, token, github_repo_data);
+        });
       }
-    })
+    });
   }
-}
+};
